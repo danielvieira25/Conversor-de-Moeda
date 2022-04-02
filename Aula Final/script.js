b1a67d3a4e0aef34418185695c0b6769aa885c3f
Daniel Vieira <101463567+danielvieira25@users.noreply.github.com>
2022-04-02 11:31:06 -0300

    Add files via upload

---
+++ Aula Final/script.js
@@ -0,0 +1,19 @@
+function Converter(){
+
+    var valorElemento =document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmDolarNumerico = parseFloat(valor)
+
+    var valorEmReal = valorEmDolarNumerico * 4.66;
+    console.log(valorEmReal);
+
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    if(valorConvertido ==null){ 
+        var valorConvertido = "Digite um valor " + valorEmReal;
+        elementoValorConvertido.innerHTML = valorConvertido;
+    }else(valorConvertido == true)
+        var valorConvertido = "O resultado em real é R$" + valorEmReal;
+            elementoValorConvertido.innerHTML = valorConvertido;
+
+
+}
